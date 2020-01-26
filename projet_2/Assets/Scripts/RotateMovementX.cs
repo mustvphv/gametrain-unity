8f1781d8b133e8feb2329f5e45bb2291173b4e1f
mustvphv <mustaphailyes.dahmani@gmail.com>
2020-01-26 21:40:54 +0100

    ajout RotateMovementX

---
+++ projet_2/Assets/Scripts/RotateMovementX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMovement : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(50 * Time.deltaTime, 0, 0);
+        
+    }
+}
