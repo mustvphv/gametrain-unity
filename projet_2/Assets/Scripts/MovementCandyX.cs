1684efd8901c224bc6d025413dbe2b778b0298c0
mustvphv <mustaphailyes.dahmani@gmail.com>
2020-01-26 21:36:49 +0100

    ajout MovementCandyX

---
+++ projet_2/Assets/Scripts/MovementCandyX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCandyX : MonoBehaviour
+{
+    public int speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
