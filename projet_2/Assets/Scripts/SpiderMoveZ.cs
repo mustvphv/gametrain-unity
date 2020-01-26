546eb80dde17725b8d3bb2463604b30b43f092a5
mustvphv <mustaphailyes.dahmani@gmail.com>
2020-01-26 21:43:06 +0100

    ajout SpiderMoveZ

---
+++ projet_2/Assets/Scripts/SpiderMoveZ.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMoveZ : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, 1 * Time.deltaTime);
+    }
+}
